03e5f2ac9611056bec2ae8391e3249b13f72130d
t-ateya <ateyaterence@gmail.com>
2021-06-11 11:46:48 +0100

    Update landing page and handle validation

--- public/viewpage/home_page.js
+++ public/viewpage/home_page.js
@@ -11,38 +11,61 @@ import * as Util from './util.js'
 import * as ThreadPage from './thread_page.js'
 
 export function addEventListeners() {
-
     Element.menuHome.addEventListener('click', async() => {
         history.pushState(null, null, Routes.routePath.HOME)
         const label = Util.disableButton(Element.menuHome)
-        home_page()
+        await home_page()
         Util.enableButton(Element.menuHome, label)
     })
 
     // Event listener for creating a new thread
     Element.formCreateThread.addEventListener('submit', async e => {
         e.preventDefault()
-        const button = Element.formCreateThread.getElementsByTagName('button')[0]
-        const label = Util.disableButton(button)
+        Element.formCreateThreadError.title.innerHTML = '';
+        Element.formCreateThreadError.keywords.innerHTML = '';
+        Element.formCreateThreadError.content.innerHTML = '';
+
         const uid = Auth.currentUser.uid
         const email = Auth.currentUser.email
         const timestamp = Date.now()
-        const title = Element.formCreateThread.title.value
-        const content = Element.formCreateThread.content.value
-        const keywords = Element.formCreateThread.keywords.value
+        const title = Element.formCreateThread.title.value.trim()
+        const content = Element.formCreateThread.content.value.trim()
+        const keywords = Element.formCreateThread.keywords.value.trim()
         const keywordsArray = keywords.toLowerCase().match(/\S+/g)
         const thread = new Thread({
-            uid,
-            email,
-            title,
-            keywordsArray,
-            content,
-            timestamp
-        })
+                uid,
+                email,
+                title,
+                keywordsArray,
+                content,
+                timestamp
+            })
+            // validate threads
+        let valid = true;
+        let error = thread.validate_title();
+        if (error) {
+            valid = false;
+            Element.formCreateThreadError.title.innerHTML = error;
+        }
+        error = thread.validate_keywords();
+        if (error) {
+            valid = false;
+            Element.formCreateThreadError.keywords.innerHTML = error;
+        }
+        error = thread.validate_content();
+        if (error) {
+            valid = false;
+            Element.formCreateThreadError.content.innerHTML = error;
+        }
+
+        if (!valid) {
+            return;
+        }
         try {
             const docId = await FirebaseController.addThread(thread)
+            const button = Element.formCreateThread.getElementsByTagName('button')[0]
+            const label = Util.disableButton(button)
             thread.docId = docId
-                // home_page() // we will improve later
             const trTag = document.createElement('tr')
             trTag.innerHTML = buildThreadView(thread)
             const threadBodyTag = document.getElementById('thread-body-tag')
@@ -50,13 +73,12 @@ export function addEventListeners() {
             const threadForms = document.getElementsByClassName('thread-view-form');
             ThreadPage.addThreadFormEvent(threadForms[0]);
             Element.formCreateThread.reset()
-
             Util.popupInfo('Success', 'A new thread has been added', Constant.iDmodalCreateNewThread)
         } catch (e) {
             if (Constant.DEV) console.log(e)
             Util.popupInfo('Failed to add', JSON.stringify(e), Constant.iDmodalCreateNewThread)
         }
-        home_page()
+        await home_page()
         Util.enableButton(button, label)
     })
 }
@@ -76,7 +98,6 @@ export async function home_page() {
         return
     }
     buildHomeScreen(threadList, true)
-
 }
 
 export function buildHomeScreen(threadList, newButton) {
@@ -84,31 +105,23 @@ export function buildHomeScreen(threadList, newButton) {
     let html = ''
     if (newButton) {
         html = `
-        <center>
-        <h4>
-        Hello <br/>
-        ${Auth.currentUser.email}<br />
-        </h4>
-        <button class="btn btn-outline-danger" data-toggle="modal" data-target="#${Constant.iDmodalCreateNewThread}">+New Thread</button>
-        </center>
-        <br />
+        <button class="btn btn-outline-danger mb-3" data-toggle="modal" data-target="#${Constant.iDmodalCreateNewThread}">+New Thread</button>
         `
     }
 
     html += `
     <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Action</th>
-      <th scope="col">Title</th>
-      <th scope="col">Keywords</th>
-      <th scope="col">Posted By</th>
-      <th scope="col">Content</th>
-      <th scope="col">Posted At</th>
-    </tr>
-  </thead>
-  <tbody id="thread-body-tag">
-    `
+        <thead>
+            <tr>
+                <th scope="col">Action</th>
+                <th scope="col">Title</th>
+                <th scope="col">Keywords</th>
+                <th scope="col">Posted By</th>
+                <th scope="col">Content</th>
+                <th scope="col">Posted At</th>
+            </tr>
+        </thead>
+    <tbody id="thread-body-tag">`
 
     threadList.forEach(thread => {
         html += '<tr>' + buildThreadView(thread) + '</tr>'
@@ -122,7 +135,6 @@ export function buildHomeScreen(threadList, newButton) {
     }
 
     Element.mainContent.innerHTML = html
-
     ThreadPage.addThreadViewEvents()
 }
 
@@ -134,10 +146,9 @@ function buildThreadView(thread) {
                     <input type="hidden" name="threadId" value="${thread.docId}">
                     <button type="submit" class ="btn btn-outline-primary">View</button>
                 </form>
-            </td>  
-                      
+            </td>               
             <td>${thread.title}</td>
-            <td>${thread.keywordsArray.join(' ')}</td>
+            <td>${!thread.keywordsArray || !Array.isArray(thread.keywordsArray) ? '' : thread.keywordsArray.join(' ')}</td>
             <td>${thread.email}</td>
             <td>${thread.content}</td>
             <td>${new Date(thread.timestamp).toString()}</td>
@@ -152,13 +163,15 @@ function buildThreadView(thread) {
                 </form>
             </td>  
                       
+            <td>
+            <button disabled class="btn btn-outline-danger" data-toggle="modal" data-target="#${Constant.iDmodalDeleteForm}">Delete</button>
+            </td>                 
             <td>${thread.title}</td>
-            <td>${thread.keywordsArray.join(' ')}</td>
+            <td>${!thread.keywordsArray || !Array.isArray(thread.keywordsArray) ? '' : thread.keywordsArray.join(' ')}</td>
             <td>${thread.email}</td>
             <td>${thread.content}</td>
             <td>${new Date(thread.timestamp).toString()}</td>
                     
     `
-
     }
 }
